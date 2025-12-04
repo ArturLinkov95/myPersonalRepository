b8e7244e9beeb04cb90aa8607105a2034bdc6462
ArturLinkov95 <__email@example.com>
2025-12-04 23:59:45 +0300

    Выполнил задание 18

--- main.js
+++ main.js
@@ -1,52 +1,32 @@
-const phoneBookOfContinental = {
-    newYork: {
-        "John Wick": 89649874694,
-        "Viggo Tarasov": 89763679877,
-        "Marcus": 89748763543,
-        "Winston": 89749837495,
-        "Charon": 89764563911
-    },
-    tokyo: {
-        "Shimazu": 89758749887,
-        "Akira": 89750947854
-    },
-    addInNewYork(name, number) {
-        this.newYork[name] = number;
-    },
-    addInTokyo(name, number) {
-        this.tokyo[name] = number;
-    },
-    deleteInNewYork(name) {
-        delete this.newYork[name];
-    },
-    deleteTokyo(name) {
-        delete this.newYork[name];
-    }
+const list = {};
+
+function showList() {
+    console.log(list);
 };
 
-phoneBookOfContinental.addInNewYork("Ms. Perkins", 89648749834);
-
-console.log(phoneBookOfContinental.newYork['Ms. Perkins']);
-
-phoneBookOfContinental.deleteInNewYork("Marcus");
-
-console.log(phoneBookOfContinental.newYork);
-
-phoneBookOfContinental.addInTokyo("Nobody", 89487649837);
-
-console.log(phoneBookOfContinental.tokyo);
-
-console.log("Akira" in phoneBookOfContinental.tokyo);
-
-console.log("Akira" in phoneBookOfContinental.newYork);
-
-for (const name in phoneBookOfContinental.newYork) {
-    console.log(name, "-", phoneBookOfContinental.newYork[name]);
+function addTask(name) {
+    list[name] = "To Do";
 };
 
-console.log(" ");
+addTask("Прочесть книгу");
+addTask("Вымыть пол");
+addTask("Полить цветы");
+addTask("Прогуляться");
 
+showList();
 
-for (const name in phoneBookOfContinental.tokyo) {
-    console.log(name, "-", phoneBookOfContinental.tokyo[name]);
+function changeStatus(name, status) {
+    list[name] = status;
 };
+
+changeStatus("Прочесть книгу", "Done");
+
+showList();
+
+function deleteTask(name) {
+    delete list[name];
+};
+
+deleteTask("Прогуляться");
+
+showList();
