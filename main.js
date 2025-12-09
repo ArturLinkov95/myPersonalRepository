da1aa06cdec529bdc7da19248db6e4a68a23d73b
ArturLinkov95 <__email@example.com>
2025-12-09 20:54:58 +0300

    Сделал задание 20, переписав функции в стрелочные

--- main.js
+++ main.js
@@ -1,82 +1,101 @@
-const todo = {
-    list: {},
-    status: {
-        "ToDo": "To Do",
-        "InProgress": "In Progress",
-        "Done": "Done"
-    },
-    statusToDo(statusName, displayName) {
-        console.log(`"${displayName}:"`)
-        let foundTask = false;
-        for (const name in this.list) {
-            if (this.list[name] === this.status[statusName]) {
-                console.log(`    "${name}"`)
-                foundTask = true;
-            }
-        }
-        if (foundTask !== true) {
-            console.log("    -")
-        }
-    },
-    // statusInProgress() {
-    //     console.log("In Progress:")
-    //     let foundTask = false;
-    //     for (const name in this.list) {
-    //         if (this.list[name] === this.status["InProgress"]) {
-    //             console.log(`    "${name}"`)
-    //             foundTask = true;
-    //         }
-    //     }
-    //     if (foundTask !== true) {
-    //         console.log("    -")
-    //     }
-    // },
-    // statusDone() {
-    //     console.log("Done:")
-    //     let foundTask = false;
-    //     for (const name in this.list) {
-    //         if (this.list[name] === this.status["Done"]) {
-    //             console.log(`    "${name}"`);
-    //             foundTask = true;
-    //         }
-    //     }
-    //     if (foundTask !== true) {
-    //         console.log("    -")
-    //     }
-    // },
-    showList() {
-        todo.statusToDo("ToDo", "To Do");
-        todo.statusToDo("InProgress", "In Progress");
-        todo.statusToDo("Done", "Done");
-    },
-    addTask(name) {
-        if (name.length < 25 && !(name in this.list)) {
-            this.list[name] = "To Do";
-        }
-        else { }
-    },
-    changeStatus(name, status) {
-        if (this.list[name]) {
-            this.list[name] = status;
-        }
-        else { }
-    },
-    deleteTask(name) {
-        delete this.list[name];
+const weather = (temp) => {
+    if (temp < 0) {
+        console.log("Опасно холодно!")
+    }
+    else if (temp >= 0 && temp <= 5) {
+        console.log("Очень холодно")
+    }
+    else if (temp >= 5 && temp <= 20) {
+        console.log("Прохладно")
+    }
+    else if (temp > 20) {
+        console.log("Тепло")
     }
 };
 
-todo.addTask("Почитать \"Преступление и наказание\"");
-todo.addTask("Вымыть пол");
-todo.addTask("Полить цветы");
-todo.addTask("Прогуляться");
+weather(21);
 
-todo.changeStatus("Прочесть книгу", "Done");
+const discount = (count) => {
+    if (count > 1000 && count < 5000) {
+        console.log(count = count - count * 5 / 100)
+    }
+    else if (count > 5000) {
+        console.log(count = count - count * 10 / 100)
+    }
+}
 
-todo.changeStatus("Вымыть пол", "Done");
+discount(5001);
 
-todo.deleteTask("Прогуляться");
+const userAge = (age) => {
+    if (age <= 17) {
+        console.log("подросток")
+    }
+    else if (age >= 17 && age <= 64) {
+        console.log("взрослый")
+    }
+    else {
+        console.log("пенсионер")
+    }
+}
 
-todo.addTask("Полить цветы");
+userAge(100);
 
-todo.showList();
+const operatingMode1 = (time) => {
+    if (time >= 9 && time <= 18) {
+        console.log("Магазин открыт")
+    }
+    else {
+        console.log("Магазин закрыт")
+    }
+}
+
+operatingMode1(19);
+
+const operatingMode2 = (time) => time >= 9 && time <= 18 ? console.log("Магазин открыт") : console.log("Магазин закрыт");
+
+operatingMode2(19);
+
+const testRating = (score) => {
+    if (score <= 100 && score >= 90) {
+        console.log("Отлично")
+    }
+    else if (score <= 89 && score >= 70) {
+        console.log("Хорошо")
+    }
+    else if (score <= 69 && score >= 50) {
+        console.log("Удовлетворительно")
+    }
+    else if (score <= 49 && score >= 0) {
+        console.log("Неудачно")
+    }
+    else (console.log("Некорректное значение"))
+};
+
+testRating(101);
+
+const checkAge = (age) => {
+    if (age < 18) {
+        console.log("you are not allowed")
+    }
+    if (age >= 18) {
+        console.log("you are welcome!")
+    }
+}
+
+checkAge(18);
+
+const сalc = (operation, a, b) => {
+    if (operation === "add") {
+        console.log(a + b)
+    }
+    else if (operation === "multi") {
+        console.log(a - b)
+    }
+    else if (operation === "subtract") {
+        console.log(a * b)
+    }
+}
+
+сalc("add", 37, 15);
+сalc("multi", 72, 17);
+сalc("subtract", 16, 8);
